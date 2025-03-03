584cb868440c4f337bf8220ba499c529659ab6e1
Gideon22iboyi <your-email@example.com>
2025-03-03 04:16:26 +0100

    fixed render link issues

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,14 +1,23 @@
 import Header from "../components/Header";
 import SignUpForm from "../components/signupform";
 import Welcome from "../components/welcome";
+import { Link } from "react-router-dom";
 
 
 function SignUpPage() {
      return(
-        <div className="flex-col flex items-center gap-7 ">
+        <div className="flex-col flex items-center gap-6 ">
             <Header />
             <Welcome />
             <SignUpForm />
+            <div className="mt-1 text-center">
+                <p className="text-[14px] text-gray-600">
+                    Already have an account?{" "}
+                    <Link to="/login" className="text-blue-600 hover:text-blue-700">
+                        Login here
+                    </Link>
+                </p>
+            </div>
         </div>
      )
 }
